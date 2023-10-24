10f72f8aa32fba4f3ff6e71877f4510a6fa8fc02
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-25 01:20:53 +0300

    Caursel Books

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
 import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
+import { Carousel } from './layouts/HomePage/Carousel';
 
 function App() {
   return (
     <div>
       <Navbar />
       <ExploreTopBooks />
+      <Carousel />
       <Footer />
     </div>
   );
