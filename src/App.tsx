f89eccd9f858c0192562649131b41778d7550b99
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-25 01:14:47 +0300

    Explore Books

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
+import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <ExploreTopBooks />
       <Footer />
     </div>
   );
