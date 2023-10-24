61d5199efc22e41b04debe543d208a4de10eb46c
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-25 01:29:03 +0300

    HomePage

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,13 @@ import React from 'react';
 import './App.css';
 import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 import { Footer } from './layouts/NavbarAndFooter/Footer';
-import { ExploreTopBooks } from './layouts/HomePage/ExploreTopBooks';
-import { Carousel } from './layouts/HomePage/Carousel';
-import { LibraryServices } from './layouts/HomePage/LibraryServices';
-import { Heros } from './layouts/HomePage/Heros';
+import { HomePage } from './layouts/HomePage/HomePage';
 
 function App() {
   return (
     <div>
       <Navbar />
-      <ExploreTopBooks />
-      <Carousel />
-      <Heros />
-      <LibraryServices />
+      <HomePage />
       <Footer />
     </div>
   );
