043cb7f7ac352c9a0aa440a2e51b48631aca8077
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-25 01:08:29 +0300

    navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
+import { Navbar } from './layouts/NavbarAndFooter/Navbar';
 
 function App() {
   return (
-    <div className='App'>
-      <h1>Welcome</h1>
-      <h2>W</h2>
+    <div>
+      <Navbar />
     </div>
   );
 }
