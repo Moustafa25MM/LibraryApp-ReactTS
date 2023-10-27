0f053966e9b8382139fb2a59e20c3dcf68f6331a
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-27 04:08:03 +0200

    Renew loan

--- src/layouts/ShelfPage/components/Loans.tsx
+++ src/layouts/ShelfPage/components/Loans.tsx
@@ -71,6 +71,21 @@ export const Loans = () => {
     }
     setCheckout(!checkout);
   }
+  async function renewLoan(bookId: number) {
+    const url = `http://localhost:8000/api/books/secure/renew/loan?bookId=${bookId}`;
+    const requestOptions = {
+      method: 'PUT',
+      headers: {
+        Authorization: `Bearer ${authState?.accessToken?.accessToken}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const returnResponse = await fetch(url, requestOptions);
+    if (!returnResponse.ok) {
+      throw new Error('Something went wrong!');
+    }
+    setCheckout(!checkout);
+  }
   return (
     <div>
       {/* Desktop */}
@@ -151,6 +166,7 @@ export const Loans = () => {
                   shelfCurrentLoan={shelfCurrentLoan}
                   mobile={false}
                   returnBook={returnBook}
+                  renewLoan={renewLoan}
                 />
               </div>
             ))}
@@ -242,6 +258,7 @@ export const Loans = () => {
                   shelfCurrentLoan={shelfCurrentLoan}
                   mobile={true}
                   returnBook={returnBook}
+                  renewLoan={renewLoan}
                 />
               </div>
             ))}
