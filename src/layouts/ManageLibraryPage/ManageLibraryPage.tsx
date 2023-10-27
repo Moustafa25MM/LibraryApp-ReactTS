97ed893df2cb166f408f043534e48c11e0b6fffc
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-27 19:19:13 +0200

    State For Manage Library

--- src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
+++ src/layouts/ManageLibraryPage/ManageLibraryPage.tsx
@@ -8,6 +8,18 @@ export const ManageLibraryPage = () => {
     useState(false);
   const [messagesClick, setMessagesClick] = useState(false);
 
+  function addBookClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(false);
+  }
+  function changeQuantityOfBooksClickFunction() {
+    setChangeQuantityOfBooksClick(true);
+    setMessagesClick(false);
+  }
+  function MessageClickFunction() {
+    setChangeQuantityOfBooksClick(false);
+    setMessagesClick(true);
+  }
   return (
     <div className='container'>
       <div className='mt-5'>
@@ -15,6 +27,7 @@ export const ManageLibraryPage = () => {
         <nav>
           <div className='nav nav-tabs' id='nav-tab' role='tablist'>
             <button
+              onClick={addBookClickFunction}
               className='nav-link active'
               id='nav-add-book-tab'
               data-bs-toggle='tab'
@@ -27,6 +40,7 @@ export const ManageLibraryPage = () => {
               Add new book
             </button>
             <button
+              onClick={changeQuantityOfBooksClickFunction}
               className='nav-link'
               id='nav-quantity-tab'
               data-bs-toggle='tab'
@@ -39,6 +53,7 @@ export const ManageLibraryPage = () => {
               Change quantity
             </button>
             <button
+              onClick={MessageClickFunction}
               className='nav-link'
               id='nav-messages-tab'
               data-bs-toggle='tab'
@@ -67,7 +82,7 @@ export const ManageLibraryPage = () => {
             role='tabpanel'
             aria-labelledby='nav-quantity-tab'
           >
-            Change Quantity
+            {changeQuantityOfBooksClick ? <>Change Quantity</> : <></>}
           </div>
           <div
             className='tab-pane fade'
@@ -75,7 +90,7 @@ export const ManageLibraryPage = () => {
             role='tabpanel'
             aria-labelledby='nav-messages-tab'
           >
-            Admin Messages
+            {messagesClick ? <>Admin Messages</> : <></>}
           </div>
         </div>
       </div>
