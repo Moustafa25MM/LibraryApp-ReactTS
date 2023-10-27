7dd2dd5322c19cd4635f4b941650fedc39ce1065
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-27 19:13:46 +0200

    manage Library Page

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import LoginWidget from './Auth/LoginWidget';
 import { ReviewListPage } from './BookCheckoutPage/ReviewListPage/ReviewListPage';
 import { ShelfPage } from './layouts/ShelfPage/ShelfPage';
 import { MessagePage } from './layouts/MessagePage/MessagePage';
+import { ManageLibraryPage } from './layouts/ManageLibraryPage/ManageLibraryPage';
 
 const oktaAuth = new OktaAuth(oktaConfig);
 function App() {
@@ -60,6 +61,9 @@ function App() {
             </SecureRoute>
             <SecureRoute path={'/messages'}>
               <MessagePage />
+            </SecureRoute>
+            <SecureRoute path={'/admin'}>
+              <ManageLibraryPage />
             </SecureRoute>
           </Switch>
         </div>
