9ce6532fa5ba43ef0a46907d7bf53689774c96ee
Moustafa25MM <107191669+Moustafa25MM@users.noreply.github.com>
2023-10-28 12:58:25 +0200

    HTTPS Configurations

--- src/lib/oktaConfig.ts
+++ src/lib/oktaConfig.ts
@@ -1,7 +1,7 @@
 export const oktaConfig = {
   clientId: '0oacy37evxw4CBYZL5d7',
   issuer: 'https://dev-74151017.okta.com/oauth2/default',
-  redirectUri: 'http://localhost:3000/login/callback',
+  redirectUri: 'https://localhost:3000/login/callback',
   scopes: ['openid', 'profile', 'email'],
   pkce: true,
   disableHttpsCheck: true,
